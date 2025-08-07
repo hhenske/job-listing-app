3b9edf2f9e8ca0e2798f216f191ba87c99abe130
Holly Henske <hcover333@gmail.com>
2025-08-07 14:55:40 -0500

    First commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/job-listing-app/', // <-- Replace this with your actual GitHub repo name
+})
